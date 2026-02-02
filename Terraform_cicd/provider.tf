aeba3c14b0c4db81d564a3aa54324673d8ee9011
Moonlight15-fireandlight <paul.pillhuaman@unmsm.edu.pe>
2026-02-01 19:00:48 -0500

    change new version awsprovider

--- Terraform_cicd/provider.tf
+++ Terraform_cicd/provider.tf
@@ -5,7 +5,7 @@ terraform {
     aws = {
       source  = "hashicorp/aws"
       #version = "~> 5.84.0" #actualizo la version de 5.72 a 5.73 con un terraform init -upgrade
-      version = "6.7.0"
+      version = "6.30.0"
       #configuration_aliases = [ aws.case1, aws.case2 ]
     }
 
@@ -16,7 +16,6 @@ terraform {
     key    = "terraform/terraform.tfstate"
     region = "us-west-2"
   }
-
 
   required_version = ">= 1.7.4" # version de terraform, verificar si la action de github lo utiliza o instala el ultimo
 
